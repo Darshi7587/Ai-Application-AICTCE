297bd6946dad869dd46cc4253f170be0d1a48c9e
Pancham-Ayush <panchamayush@gmail.com>
2026-03-17 21:57:48 +0530

    Add Spring Boot backend with entity models and pom configuration

---
+++ BackEnd/src/main/java/com/example/edumap/Entity/Course.java
@@ -0,0 +1,30 @@
+package com.example.edumap.Entity;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Data
+@Getter
+@Setter
+public class Course {
+
+    @Id
+    private String courseCode;
+
+    private String courseName;
+
+    private String courseDescription;
+
+    @OneToMany(mappedBy = "course",cascade = CascadeType.ALL)
+    @JsonManagedReference
+    List<CourseOutcomes> COes = new ArrayList<>();
+
+}
